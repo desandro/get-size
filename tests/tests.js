8319efd0c69325b434bde44c601308bbe570720f
David DeSandro <desandrocodes@gmail.com>
2012-12-20 10:40:07 -0500

    simple tests width: 100%, height 50%

--- tests/tests.js
+++ tests/tests.js
@@ -15,14 +15,26 @@ function getBoxSize( num ) {
   return getSize( box );
 }
 
-test( 'bad argument', function() {
+test( 'bad arguments', function() {
   equal( getSize( 0 ), null, 'Number returns null' );
 });
 
-test( 'no styling', function() {
+test( 'ex1: no styling', function() {
   var size = getBoxSize(1);
   equal( size.width, 400, 'Inherit container width' );
   equal( size.height, 0, 'No height' );
 });
+
+test( 'ex2: height: 100%', function() {
+  var size = getBoxSize(2);
+  equal( size.height, 200, 'Inherit height' );
+});
+
+test( 'ex3: width: 50%; height: 50%', function() {
+  var size = getBoxSize(3);
+  equal( size.width, 200, 'half width' );
+  equal( size.height, 100, 'half height' );
+});
+
 
 })( window );
