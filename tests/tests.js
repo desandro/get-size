8792e46b2fd185878579d6ef93162d40ba8aede2
David DeSandro <desandrocodes@gmail.com>
2012-12-20 10:28:45 -0500

    basic tests

--- tests/tests.js
+++ tests/tests.js
@@ -1,7 +1,28 @@
+/**
+ * getSize tests
+ * with QUnit
+**/
+
+/*jshint browser: true, devel: true, strict: true, undef: true */
+/*global equal: false, getSize: false, ok: false, test: false */
+
 ( function( window ) {
 
-  test( 'my test', function() {
-    ok( true, 'yes, its true' );
-  });
+'use strict';
+
+function getBoxSize( num ) {
+  var box = document.querySelector( '#ex' + num + ' .box' );
+  return getSize( box );
+}
+
+test( 'bad argument', function() {
+  equal( getSize( 0 ), null, 'Number returns null' );
+});
+
+test( 'no styling', function() {
+  var size = getBoxSize(1);
+  equal( size.width, 400, 'Inherit container width' );
+  equal( size.height, 0, 'No height' );
+});
 
 })( window );
