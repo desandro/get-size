0d4d2e3bd02ec91772923f9d26a4c858ccf67953
David DeSandro <david@desandro.com>
2013-01-13 15:42:38 -0500

    more tests with padding, margins

--- tests/tests.js
+++ tests/tests.js
@@ -15,6 +15,10 @@ function getBoxSize( num ) {
   return getSize( box );
 }
 
+window.onload = function() {
+
+
+
 test( 'bad arguments', function() {
   equal( getSize( 0 ), null, 'Number returns null' );
 });
@@ -36,5 +40,54 @@ test( 'ex3: width: 50%; height: 50%', function() {
   equal( size.height, 100, 'half height' );
 });
 
+test( 'ex4: border: 10px solid', function() {
+  var size = getBoxSize(4);
+  // console.log( size );
+  equal( size.width, 220, 'width = 200 width + 10 border + 10 border' );
+  equal( size.height, 120, 'height = 100 height + 10 border + 10 border' );
+  equal( size.innerWidth, 200, 'innerWidth = 200 width' );
+  equal( size.innerHeight, 100, 'innerHeight = 200 width' );
+  equal( size.outerWidth, 220, 'outerWidth = 200 width + 10 border + 10 border' );
+  equal( size.outerHeight, 120, 'outerHeight = 100 height + 10 border + 10 border' );
+});
+
+test( 'ex5: border: 10px solid; margin: 15px', function() {
+  var size = getBoxSize(5);
+  // console.log( size );
+  equal( size.width, 220, 'width = 200 width + 10 border + 10 border' );
+  equal( size.height, 120, 'height = 100 height + 10 border + 10 border' );
+  equal( size.innerWidth, 200, 'innerWidth = 200 width' );
+  equal( size.innerHeight, 100, 'innerHeight = 200 width' );
+  equal( size.outerWidth, 250, 'outerWidth = 200 width + 20 border + 30 margin' );
+  equal( size.outerHeight, 150, 'outerHeight = 100 height + 20 border + 30 margin' );
+});
+
+test( 'ex6: padding, set width/height', function() {
+  var size = getBoxSize(6);
+  // console.log( size );
+  equal( size.width, 260, 'width' );
+  equal( size.height, 140, 'height' );
+  equal( size.innerWidth, 200, 'innerWidth = 400 width - 20 padding - 40 padding' );
+  equal( size.innerHeight, 100, 'innerHeight = 200 height - 10 padding - 30 padding' );
+  equal( size.outerWidth, 260, 'outerWidth' );
+  equal( size.outerHeight, 140, 'outerHeight' );
+
+});
+
+test( 'ex7: padding, inherit width', function() {
+  var size = getBoxSize(7);
+  // console.log( size );
+  equal( size.width, 400, 'width' );
+  equal( size.height, 140, 'height' );
+  equal( size.innerWidth, 340, 'innerWidth = 400 width - 20 padding - 40 padding' );
+  equal( size.innerHeight, 100, 'innerHeight = 200 height - 10 padding - 30 padding' );
+  equal( size.outerWidth, 400, 'outerWidth' );
+  equal( size.outerHeight, 140, 'outerHeight' );
+
+});
+
+
+
+}; // onload
 
 })( window );
