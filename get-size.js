cb6b572eb1a8b393a6c889496ed1fcb43ef66458
Dave DeSandro <desandrocodes@gmail.com>
2021-12-19 21:28:39 -0500

    👔 add ESLint; fix lints

--- get-size.js
+++ get-size.js
@@ -1,18 +1,11 @@
 /*!
- * getSize v2.0.3
+ * getSize v3.0.0
  * measure size of elements
  * MIT license
  */
 
-/* jshint browser: true, strict: true, undef: true, unused: true */
-/* globals console: false */
-
 ( function( window, factory ) {
-  /* jshint strict: false */ /* globals define, module */
-  if ( typeof define == 'function' && define.amd ) {
-    // AMD
-    define( factory );
-  } else if ( typeof module == 'object' && module.exports ) {
+  if ( typeof module == 'object' && module.exports ) {
     // CommonJS
     module.exports = factory();
   } else {
@@ -20,29 +13,28 @@
     window.getSize = factory();
   }
 
-})( window, function factory() {
-'use strict';
+} )( window, function factory() {
 
 // -------------------------- helpers -------------------------- //
 
 // get a number from a string, not a percentage
 function getStyleSize( value ) {
-  var num = parseFloat( value );
+  let num = parseFloat( value );
   // not a percent like '100%', and a number
-  var isValid = value.indexOf('%') == -1 && !isNaN( num );
+  let isValid = value.indexOf('%') == -1 && !isNaN( num );
   return isValid && num;
 }
 
 function noop() {}
 
-var logError = typeof console == 'undefined' ? noop :
+let logError = typeof console == 'undefined' ? noop :
   function( message ) {
     console.error( message );
   };
 
 // -------------------------- measurements -------------------------- //
 
-var measurements = [
+let measurements = [
   'paddingLeft',
   'paddingRight',
   'paddingTop',
@@ -54,22 +46,22 @@ var measurements = [
   'borderLeftWidth',
   'borderRightWidth',
   'borderTopWidth',
-  'borderBottomWidth'
+  'borderBottomWidth',
 ];
 
-var measurementsLength = measurements.length;
+let measurementsLength = measurements.length;
 
 function getZeroSize() {
-  var size = {
+  let size = {
     width: 0,
     height: 0,
     innerWidth: 0,
     innerHeight: 0,
     outerWidth: 0,
-    outerHeight: 0
+    outerHeight: 0,
   };
-  for ( var i=0; i < measurementsLength; i++ ) {
-    var measurement = measurements[i];
+  for ( let i = 0; i < measurementsLength; i++ ) {
+    let measurement = measurements[i];
     size[ measurement ] = 0;
   }
   return size;
@@ -77,12 +69,10 @@ function getZeroSize() {
 
 // -------------------------- getStyle -------------------------- //
 
-/**
- * getStyle, get style of element, check for Firefox bug
- * https://bugzilla.mozilla.org/show_bug.cgi?id=548397
- */
+// getStyle, get style of element, check for Firefox bug
+// https://bugzilla.mozilla.org/show_bug.cgi?id=548397
 function getStyle( elem ) {
-  var style = getComputedStyle( elem );
+  let style = getComputedStyle( elem );
   if ( !style ) {
     logError( 'Style returned ' + style +
       '. Are you running this code in a hidden iframe on Firefox? ' +
@@ -93,9 +83,9 @@ function getStyle( elem ) {
 
 // -------------------------- setup -------------------------- //
 
-var isSetup = false;
+let isSetup = false;
 
-var isBoxSizeOuter;
+let isBoxSizeOuter;
 
 /**
  * setup
@@ -115,16 +105,16 @@ function setup() {
    * Chrome & Safari measure the outer-width on style.width on border-box elems
    * IE11 & Firefox<29 measures the inner-width
    */
-  var div = document.createElement('div');
+  let div = document.createElement('div');
   div.style.width = '200px';
   div.style.padding = '1px 2px 3px 4px';
   div.style.borderStyle = 'solid';
   div.style.borderWidth = '1px 2px 3px 4px';
   div.style.boxSizing = 'border-box';
 
-  var body = document.body || document.documentElement;
+  let body = document.body || document.documentElement;
   body.appendChild( div );
-  var style = getStyle( div );
+  let style = getStyle( div );
   // round value for browser zoom. desandro/masonry#928
   isBoxSizeOuter = Math.round( getStyleSize( style.width ) ) == 200;
   getSize.isBoxSizeOuter = isBoxSizeOuter;
@@ -147,46 +137,46 @@ function getSize( elem ) {
     return;
   }
 
-  var style = getStyle( elem );
+  let style = getStyle( elem );
 
   // if hidden, everything is 0
   if ( style.display == 'none' ) {
     return getZeroSize();
   }
 
-  var size = {};
+  let size = {};
   size.width = elem.offsetWidth;
   size.height = elem.offsetHeight;
 
-  var isBorderBox = size.isBorderBox = style.boxSizing == 'border-box';
+  let isBorderBox = size.isBorderBox = style.boxSizing == 'border-box';
 
   // get all measurements
-  for ( var i=0; i < measurementsLength; i++ ) {
-    var measurement = measurements[i];
-    var value = style[ measurement ];
-    var num = parseFloat( value );
+  for ( let i = 0; i < measurementsLength; i++ ) {
+    let measurement = measurements[i];
+    let value = style[ measurement ];
+    let num = parseFloat( value );
     // any 'auto', 'medium' value will be 0
     size[ measurement ] = !isNaN( num ) ? num : 0;
   }
 
-  var paddingWidth = size.paddingLeft + size.paddingRight;
-  var paddingHeight = size.paddingTop + size.paddingBottom;
-  var marginWidth = size.marginLeft + size.marginRight;
-  var marginHeight = size.marginTop + size.marginBottom;
-  var borderWidth = size.borderLeftWidth + size.borderRightWidth;
-  var borderHeight = size.borderTopWidth + size.borderBottomWidth;
+  let paddingWidth = size.paddingLeft + size.paddingRight;
+  let paddingHeight = size.paddingTop + size.paddingBottom;
+  let marginWidth = size.marginLeft + size.marginRight;
+  let marginHeight = size.marginTop + size.marginBottom;
+  let borderWidth = size.borderLeftWidth + size.borderRightWidth;
+  let borderHeight = size.borderTopWidth + size.borderBottomWidth;
 
-  var isBorderBoxSizeOuter = isBorderBox && isBoxSizeOuter;
+  let isBorderBoxSizeOuter = isBorderBox && isBoxSizeOuter;
 
   // overwrite width and height if we can get it from style
-  var styleWidth = getStyleSize( style.width );
+  let styleWidth = getStyleSize( style.width );
   if ( styleWidth !== false ) {
     size.width = styleWidth +
       // add padding and border unless it's already including it
       ( isBorderBoxSizeOuter ? 0 : paddingWidth + borderWidth );
   }
 
-  var styleHeight = getStyleSize( style.height );
+  let styleHeight = getStyleSize( style.height );
   if ( styleHeight !== false ) {
     size.height = styleHeight +
       // add padding and border unless it's already including it
@@ -204,4 +194,4 @@ function getSize( elem ) {
 
 return getSize;
 
-});
+} );
