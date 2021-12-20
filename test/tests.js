43972d8b296c226cc02556b5493515d057580bc1
Dave DeSandro <desandrocodes@gmail.com>
2021-12-19 21:45:08 -0500

    lint tests.js

--- test/tests.js
+++ test/tests.js
@@ -3,44 +3,42 @@
  * with QUnit
 **/
 
-/*jshint browser: true, devel: true, strict: true, undef: true */
-/*global equal: false, getSize: false, ok: false, test: false, strictEqual: false */
+/* globals equal, getSize, ok, test, strictEqual */
 
-( function( window ) {
-
-'use strict';
+( function() {
 
 function getBoxSize( num ) {
-  var box = document.querySelector( '#ex' + num + ' .box' );
+  let box = document.querySelector( '#ex' + num + ' .box' );
   return getSize( box );
 }
 
 test( 'arguments', function() {
   ok( !getSize( 0 ), 'Number returns falsey' );
-  ok( !getSize( document.querySelector('#foobabbles') ), 'bad querySelector returns falsey' );
+  ok( !getSize( document.querySelector('#foobabbles') ),
+      'bad querySelector returns falsey' );
   ok( getSize('#ex1'), 'query selector string works' );
-});
+} );
 
 test( 'ex1: no styling', function() {
-  var size = getBoxSize(1);
+  let size = getBoxSize( 1 );
   equal( size.width, 400, 'Inherit container width' );
   equal( size.height, 0, 'No height' );
   equal( size.isBorderBox, false, 'isBorderBox' );
-});
+} );
 
 test( 'ex2: height: 100%', function() {
-  var size = getBoxSize(2);
+  let size = getBoxSize( 2 );
   equal( size.height, 200, 'Inherit height' );
-});
+} );
 
 test( 'ex3: width: 50%; height: 50%', function() {
-  var size = getBoxSize(3);
+  let size = getBoxSize( 3 );
   equal( size.width, 200, 'half width' );
   equal( size.height, 100, 'half height' );
-});
+} );
 
 test( 'ex4: border: 10px solid', function() {
-  var size = getBoxSize(4);
+  let size = getBoxSize( 4 );
   // console.log( size );
   equal( size.width, 220, 'width = 220 width' );
   equal( size.height, 120, 'height = 120 height' );
@@ -48,11 +46,11 @@ test( 'ex4: border: 10px solid', function() {
   equal( size.innerHeight, 100, 'innerHeight = 200 width' );
   equal( size.outerWidth, 220, 'outerWidth = 200 width + 10 border + 10 border' );
   equal( size.outerHeight, 120, 'outerHeight = 100 height + 10 border + 10 border' );
-});
+} );
 
 test( 'ex5: border: 10px solid; margin: 15px', function() {
   // margin: 10px 20px 30px 40px;
-  var size = getBoxSize(5);
+  let size = getBoxSize( 5 );
   // console.log( size );
   equal( size.width, 220, 'width = 220 width' );
   equal( size.height, 120, 'height = 120 height' );
@@ -64,10 +62,10 @@ test( 'ex5: border: 10px solid; margin: 15px', function() {
   equal( size.innerHeight, 100, 'innerHeight = 200 width' );
   equal( size.outerWidth, 280, 'outerWidth = 200 width + 20 border + 60 margin' );
   equal( size.outerHeight, 160, 'outerHeight = 100 height + 20 border + 40 margin' );
-});
+} );
 
 test( 'ex6: padding, set width/height', function() {
-  var size = getBoxSize(6);
+  let size = getBoxSize( 6 );
   // console.log( size );
   equal( size.width, 260, 'width' );
   equal( size.height, 140, 'height' );
@@ -76,11 +74,11 @@ test( 'ex6: padding, set width/height', function() {
   equal( size.outerWidth, 260, 'outerWidth' );
   equal( size.outerHeight, 140, 'outerHeight' );
 
-});
+} );
 
 test( 'ex7: padding, inherit width', function() {
   // padding: 10px 20px 30px 40px;
-  var size = getBoxSize(7);
+  let size = getBoxSize( 7 );
   // console.log( size );
   equal( size.width, 400, 'width' );
   equal( size.height, 140, 'height' );
@@ -93,10 +91,10 @@ test( 'ex7: padding, inherit width', function() {
   equal( size.outerWidth, 400, 'outerWidth' );
   equal( size.outerHeight, 140, 'outerHeight' );
 
-});
+} );
 
 test( 'ex8: 66.666% values', function() {
-  var size = getBoxSize(8);
+  let size = getBoxSize( 8 );
 
   if ( size.width % 1 ) {
     ok( size.width > 266.6 && size.width < 266.7, 'width is between 266.6 and 266.7' );
@@ -111,10 +109,10 @@ test( 'ex8: 66.666% values', function() {
     // IE8
     equal( size.height, 133, 'width is 133' );
   }
-});
+} );
 
 test( 'ex9: border-box', function() {
-  var size = getBoxSize(9);
+  let size = getBoxSize( 9 );
   equal( size.isBorderBox, true, 'isBorderBox' );
   equal( size.width, 400, 'width' );
   equal( size.height, 200, 'height' );
@@ -122,10 +120,10 @@ test( 'ex9: border-box', function() {
   equal( size.innerHeight, 120, 'innerHeight' );
   equal( size.outerWidth, 400, 'outerWidth' );
   equal( size.outerHeight, 200, 'outerHeight' );
-});
+} );
 
 test( 'display: none', function() {
-  var size = getSize( document.querySelector('#hidden .box1') );
+  let size = getSize( document.querySelector('#hidden .box1') );
   strictEqual( size.width, 0, 'width' );
   strictEqual( size.height, 0, 'height' );
   strictEqual( size.innerWidth, 0, 'innerWidth' );
@@ -138,10 +136,10 @@ test( 'display: none', function() {
   size = getSize( document.querySelector('#hidden .box2') );
   strictEqual( size.width, 0, 'cannot over write zeroSize' );
 
-});
+} );
 
 test( 'percent values', function() {
-  var size = getSize( document.querySelector('#percent .box') );
+  let size = getSize( document.querySelector('#percent .box') );
   strictEqual( size.marginLeft, 40, 'marginLeft' );
   strictEqual( size.marginTop, 80, 'marginTop' );
   strictEqual( size.width, 200, 'width' );
@@ -150,7 +148,6 @@ test( 'percent values', function() {
   strictEqual( size.innerHeight, 100, 'innerHeight' );
   strictEqual( size.outerWidth, 240, 'outerWidth' );
   strictEqual( size.outerHeight, 180, 'outerHeight' );
-});
+} );
 
-
-})( window );
+} )( window );
